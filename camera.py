d7e4a58b9945062ef80fe9481b6952d9f9b455e2
Dmytro Sadovnychyi <git@dmit.ro>
2017-12-17 23:45:17 +0800

    Correctly remove file

--- camera.py
+++ camera.py
@@ -93,7 +93,7 @@ def upload(filename):
     media_body=MediaFileUpload(filename, chunksize=-1, resumable=True)
   ).execute()
   logging.debug('Successfully uploaded %s', result)
-  shutil.rmtree(filename)
+  os.remove(filename)
 
 
 def watch():
