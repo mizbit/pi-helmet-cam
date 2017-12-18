3a137fd875a924f41751da43eb9be27be13f2820
Dmytro Sadovnychyi <git@dmit.ro>
2017-12-18 21:08:25 +0800

    Reset socket timeout to original value

--- camera.py
+++ camera.py
@@ -86,6 +86,7 @@ def is_connected(host='8.8.8.8', port=53, timeout=1):
   try:
     socket.setdefaulttimeout(timeout)
     socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+    socket.setdefaulttimeout(None)
     return True
   except socket.error:
     return False
